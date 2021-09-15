945b9b9e0aeb13ac94abe339c98b5ca2a1fc5141
Charles Siegel <charles.m.siegel@gmail.com>
2021-09-15 07:42:15 -0700

    wrote send_tweet function

--- send_tweet.py
+++ send_tweet.py
@@ -1,2 +1,16 @@
-def send_tweet(image, text):
-    pass
+import json
+import tweepy
+
+
+def send_tweet(text, image=None):
+    with open("API_KEYS.json", 'r') as filename:
+        auths = json.load(filename)
+
+    auth = tweepy.OAuthHandler(auths['API_KEY'], auths['API_SECRET_KEY'])
+    auth.set_access_token(auths['access_token'], auths['access_token_secret'])
+
+    api = tweepy.API(auth)
+    if image is not None:
+        api.update_status(text, media_ids=[image])
+    else:
+        api.update_status(text)

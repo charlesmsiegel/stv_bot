216f23c17325108ffbcc06b4f72697139b27a114
Charles Siegel <charles.m.siegel@gmail.com>
2021-09-15 06:51:18 -0700

    structure of remainder of project

---
+++ get_info.py
@@ -0,0 +1,4 @@
+def get_info(url):
+    image = None
+    text = None
+    return image, text
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from get_urls import get_new_item_urls
+from get_info import get_info
+from send_tweet import send_tweet
 
 new_urls = get_new_item_urls()
 for url in new_urls:
     print(url)
+    image, text = get_info(url)
+    send_tweet(image, text)

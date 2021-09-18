7efe97c187d1cb1f135b01dfd41d7fb2ad7fb8a8
Charles Siegel <charles.m.siegel@gmail.com>
2021-09-17 22:42:44 -0700

    removed TODOs converting to issues

--- stv_bot/obs_scraping/get_individual_record.py
+++ stv_bot/obs_scraping/get_individual_record.py
@@ -13,13 +13,7 @@ def get_product(product_id):
     title = product['attributes']['description']['name']
     authors = product['attributes']['authors']
     price = float(product['attributes']['price'])
-    # gamelines = None  # TODO: Extract gamelines info
-    # editions = None  # TODO: Extract editions info
-    # ranking = [product['attributes']['ranking'][x]['humanName']
-    #            for x in product['attributes']['ranking'].keys()]
     authors = ", ".join(authors)
-
-    # TODO: handle PWYW titles better
 
     if authors == "":
         return f"{title} has been released at ${price:.02f}! https://www.storytellersvault.com/product/{product_id}/"
